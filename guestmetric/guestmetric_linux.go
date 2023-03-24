458ee01d705f8cb4024c3a18569822e038d69802
Yannick te Kulve <738464+YannickTeKulve@users.noreply.github.com>
2023-03-24 15:33:08 +0100

    remove unused func

--- guestmetric/guestmetric_linux.go
+++ guestmetric/guestmetric_linux.go
@@ -108,29 +108,6 @@ func enumNetworkAddresses(iface string) (GuestMetric, error) {
 	return d, nil
 }
 
-func GetInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
-	var (
-		ief      *net.Interface
-		addrs    []net.Addr
-		ipv4Addr net.IP
-	)
-	if ief, err = net.InterfaceByName(interfaceName); err != nil { // get interface
-		return
-	}
-	if addrs, err = ief.Addrs(); err != nil { // get addresses
-		return
-	}
-	for _, addr := range addrs { // get ipv4 address
-		if ipv4Addr = addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
-			break
-		}
-	}
-	if ipv4Addr == nil {
-		return "", errors.New(fmt.Sprintf("interface %s don't have an ipv4 address\n", interfaceName))
-	}
-	return ipv4Addr.String(), nil
-}
-
 func getPlainVifId(path string) (string, error) {
 	nodenamePath := fmt.Sprintf("%s/device/nodename", path)
 	strLine, err := readSysfs(nodenamePath)
