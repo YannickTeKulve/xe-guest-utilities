8bdabcc2e5e6ec349d24032e694ac0662faac681
Yannick te Kulve <738464+YannickTeKulve@users.noreply.github.com>
2023-03-29 09:45:15 +0200

    net erin

--- guestmetric/guestmetric_linux.go
+++ guestmetric/guestmetric_linux.go
@@ -94,7 +94,7 @@ func enumNetworkAddresses(iface string) (GuestMetric, error) {
 	var v4re, v6re *regexp.Regexp
 	var out string
 	var err error
-	_, _, _ := net.ParseCIDR("172.0.0.1/32")
+	net.ParseCIDR("172.0.0.1/32")
 
 	if out, err = runCmd("ip", "addr", "show", iface); err == nil {
 		v4re = IP_IPV4_ADDR_RE
